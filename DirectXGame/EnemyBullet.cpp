a2d454cb0148ad0c0e89b08478ab2d4805a78830
Otsuka Daichi <109046865+KujataD@users.noreply.github.com>
2026-05-04 12:13:05 +0900

    AL3_03_09_ex1

--- DirectXGame/EnemyBullet.cpp
+++ DirectXGame/EnemyBullet.cpp
@@ -1,5 +1,7 @@
 #include "EnemyBullet.h"
 
+using namespace KujakuEngine;
+
 void EnemyBullet::Initialize(KujakuEngine::Model* model, KujakuEngine::Camera* camera, const KujakuEngine::Vector3& position, const KujakuEngine::Vector3& velocity) {
 	assert(model);
 	assert(camera);
@@ -9,10 +11,17 @@ void EnemyBullet::Initialize(KujakuEngine::Model* model, KujakuEngine::Camera* c
 
 	worldTransform_.Initialize();
 	worldTransform_.translation_ = position;
-	worldTransform_.scale_ = {1.0f, 1.0f, 1.0f};
+	worldTransform_.scale_ = {0.5f, 0.5f, 3.0f};
 	model_->SetColor({1.0f, 0.0f, 0.0f, 1.0f});
 	velocity_ = velocity;
 	deathTimer_ = Param::lifeTime_;
+
+	// Y軸周り角度(θy) ...atan2(高さ, 底辺)
+	worldTransform_.rotation_.y = std::atan2(velocity_.x, velocity_.z); 
+	// 横軸方向の長さを求める
+	float velocityXZ = Length({velocity_.x, 0.0f, velocity_.z});
+	// X軸周り角度(θx)
+	worldTransform_.rotation_.x = std::atan2(-velocity_.y, velocityXZ);
 }
 
 void EnemyBullet::Update() {
