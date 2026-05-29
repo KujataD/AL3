844182401798516e059f2b455318782e7ea41fd5
KujataD <daichi.kujata@gmail.com>
2026-05-30 00:23:04 +0900

    xinput追加

--- DirectXGame/KujakuEngine/input/Input.h
+++ DirectXGame/KujakuEngine/input/Input.h
@@ -2,11 +2,14 @@
 #include "../math/Vector2.h"
 #include <Windows.h>
 #include <cassert>
-// dinput
-#define DIRECTINPUT_VERSION 0x0800 // DirectInputのバージョン指定
+
+// DirectInputのバージョン指定
+#define DIRECTINPUT_VERSION 0x0800
 #include <dinput.h>
+#include <Xinput.h>
 #pragma comment(lib, "dinput8.lib")
 #pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "xinput.lib")
 
 namespace KujakuEngine {
 
@@ -28,6 +31,23 @@ public:
 
 	static Vector2 GetMousePos();
 
+	// コントローラーが接続されているかを取得する。
+	static bool IsControllerConnected(int padNo = 0);
+
+	// コントローラーのボタン入力を取得する。
+	static bool GetControllerButton(WORD button, int padNo = 0);
+	static bool GetPreControllerButton(WORD button, int padNo = 0);
+	static bool GetControllerButtonTrigger(WORD button, int padNo = 0);
+	static bool GetControllerButtonRelease(WORD button, int padNo = 0);
+
+	// 左右スティックの入力を取得する。
+	static Vector2 GetLeftStick(int padNo = 0);
+	static Vector2 GetRightStick(int padNo = 0);
+
+	// 左右トリガーの入力を取得する。
+	static float GetLeftTrigger(int padNo = 0);
+	static float GetRightTrigger(int padNo = 0);
+
 private:
 	static HWND hwnd_;
 
@@ -35,13 +55,18 @@ private:
 	static IDirectInputDevice8* keyboard_;
 	static IDirectInputDevice8* mouse_;
 
-	// キー入力の箱
+	// キーボード入力状態
 	static BYTE key_[256];
 	static BYTE preKey_[256];
 
-	// マウス入力を受け止める箱
+	// マウス入力状態
 	static DIMOUSESTATE2 mouseState_;
 	static DIMOUSESTATE2 preMouseState_;
+
+	// 最大4台分のコントローラー入力状態を保持する。
+	static XINPUT_STATE controllerState_[XUSER_MAX_COUNT];
+	static XINPUT_STATE preControllerState_[XUSER_MAX_COUNT];
+	static bool isControllerConnected_[XUSER_MAX_COUNT];
 };
 
 }; // namespace KujakuEngine
